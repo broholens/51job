2b503bf447aa3431ca896c6b471569922e6074a7
broholens <broholens@outlook.com>
2019-04-09 16:02:47 +0800

    support region

--- job51.py
+++ job51.py
@@ -1,10 +1,13 @@
+import re
 import csv
 import time
+import json
 import random
 import datetime
 from urllib.parse import quote  # quote keyword
 import requests
 from lxml.html import etree  # pip install lxml
+from fuzzywuzzy import process  # pip install fuzzywuzzy
 from fake_useragent import UserAgent  # pip install fake-useragent
 # from fake_useragent.errors import FakeUserAgentError
 
@@ -15,19 +18,38 @@ except:
     pass
 
 
+def get_area_codes():
+    # request area and code mapping table.
+    url = 'https://js.51jobcdn.com/in/js/2016/layer/area_array_c.js?20180319'
+    resp = requests.get(url)
+    code_str = resp.text.split('{')[-1].split('}')[0]
+    code_str = '{' + code_str + '}'
+    codes = json.loads(code_str)
+    # convert {code: area} to {area: code}
+    codes = {j: i for i, j in codes.items()}
+    codes.update({'全国': '000000'})
+    return codes
+
+
 class JobCrawler:
     # headers
-    columns = ['education', 'job_name', 'experience', 'company flag', 'company name', 'job details', 'company trade', 'salary', 'address', 'url']
-    # search nationwide
-    url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,{},2,{}.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
-
+    columns = ['salary', 'telephone', 'education', 'job name', 'experience', 'company flag', 'company name', 'job details', 'company trade', 'location', 'addr', 'company information', 'url']
+    # search url
+    url = 'https://search.51job.com/list/{},000000,0000,00,9,99,{},2,{}.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
+    
     def __init__(self, filename='job.csv'):
         self.ua = UserAgent()
         self.f = open(filename, 'w', newline='')
         self.writer = csv.writer(self.f)
         # write headers
         self.writer.writerow(self.columns)
-
+        # match telephone
+        self.phone_ptn = re.compile('\d{11}')
+        self.tel_ptn = re.compile('[0-9-)]{12, 18}')
+        # save error url
+        self.error_f = open('error.txt', 'w')
+        # load area codes
+        self.area_codes = get_area_codes()
 
     def request(self, url):
         try:
@@ -46,9 +68,9 @@ class JobCrawler:
         page_num = int(page_txt.split('页')[0].strip('共'))
         return page_num
 
-    def generate_page_urls(self, kw, page_num):
+    def generate_page_urls(self, code, kw, page_num):
         # generate page url by keyword and total page number
-        return [self.url.format(kw, i) for i in range(1, page_num+1)]
+        return [self.url.format(code, kw, i) for i in range(1, page_num+1)]
 
     def parse_resp_to_tree(self, resp):
         try:
@@ -66,26 +88,56 @@ class JobCrawler:
         jobs_urls = tree.xpath('//div[@id="resultList"]/div[@class="el"]/p/span/a/@href')
         return jobs_urls
 
+    def extract_info_by_xp(self, tree, xp, default=''):
+        # extract information by xpath
+        info = tree.xpath(xp) or default
+        if info:
+            info = ','.join(info)
+        return info.strip().replace('\xa0', '')
+
+    def extract_info_by_regex(self, ptn, msg, default=''):
+        # extract information by regex
+        info = ptn.findall(msg) or ''
+        if info:
+            info = ','.join(info)
+        return info.strip().replace('\xa0', '')
+    
+
     def parse_job(self, tree):
         # parse job information
         title = tree.xpath('//div[@class="cn"]/p[contains(@class, "msg")]/@title')[0]
-        addr, expe, edu = [i.strip() for i in title.split('|')[:3]]
-        job_name = tree.xpath('//div[@class="cn"]/h1/@title')[0]
-        salary = tree.xpath('//div[@class="cn"]/strong/text()')
-        salary = salary[0] if salary else 'unknown'
-        com_name = tree.xpath('//a[contains(@class, "com_name")]/p/@title')[0]
+        loca, expe, edu = [i.strip() for i in title.split('|')[:3]]
+        if '招' in edu:
+            edu = ''
+        job_name = self.extract_info_by_xp(tree, '//div[@class="cn"]/h1/@title')
+        salary = self.extract_info_by_xp(tree, '//div[@class="cn"]/strong/text()')
+        if '千' in salary:
+            salary = salary.split('千')[0]
+            low, high = salary.split('-')
+            salary = str(round(float(low)/10, 1)) + '-' + str(round(float(high)/10, 1))
+        elif '万' in salary:
+            salary = salary.split('万')[0]
+        com_name = self.extract_info_by_xp(tree, '//a[contains(@class, "com_name")]/p/@title')
         com_flag, _, com_trade = tree.xpath('//div[contains(@class, "com_tag")]/p/@title')
         job_msg = tree.xpath('//div[contains(@class, "job_msg")]/p/text()')
-        job_msg = ''.join([i for i in job_msg if i.strip()])
-        return [edu, job_name, expe, com_flag, com_name, job_msg, com_trade, salary, addr]
+        job_msg = ''.join([i for i in job_msg if i.strip()]).replace('\xa0', '')
+        com_msg = self.extract_info_by_xp(tree, '//div[starts-with(@class, "tmsg")]/text()')
+        addr = self.extract_info_by_xp(tree, '//div[@class="bmsg inbox"]/p/text()')
+        phone = self.extract_info_by_regex(self.phone_ptn, job_msg+com_msg)
+        tel = self.extract_info_by_regex(self.tel_ptn, job_msg+com_msg)
+        phone = ','.join([phone, tel]).strip(',')
+        return [salary, phone, edu, job_name, expe, com_flag, com_name, job_msg, com_trade, loca, addr, com_msg]
 
-    def crawl(self, keyword):
+    def crawl(self, keyword, area='全国'):
         # crawl job information with given keyword
         quoted_kw = quote(keyword)
-        resp = self.request(self.url.format(quoted_kw, 1))
+        area = process.extractOne(area, self.area_codes.keys())[0]
+        # default is nationwide
+        code = self.area_codes.get(area, '000000')
+        resp = self.request(self.url.format(code, quoted_kw, 1))
         tree = self.parse_resp_to_tree(resp)
         page_num = self.parse_page_num(tree)
-        urls = self.generate_page_urls(quoted_kw, page_num)
+        urls = self.generate_page_urls(code, quoted_kw, page_num)
         for page_url in urls:
             # every page
             resp = self.request(page_url)
@@ -100,15 +152,17 @@ class JobCrawler:
                 tree = self.parse_resp_to_tree(resp)
                 try:
                     job_details = self.parse_job(tree)
+                    job_details.append(link)
+                    self.writer.writerow(job_details)
                     print(job_details)
                 except:
+                    self.error_f.write(link+'\n')
                     continue
-                job_details.append(link)
-                self.writer.writerow(job_details)
 
 
 if __name__ == '__main__':
-    keywords = ['统计专业', '统计应用', '统计系']
+    keywords = ['电气工程师']
+    area = '西安'
     crawler = JobCrawler()
     for x in keywords:
-        crawler.crawl(x)
+        crawler.crawl(x, area)
