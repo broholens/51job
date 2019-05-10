de334ae80119c57cba901ae00c31cbeb1edaec80
broholens <broholens@outlook.com>
2019-05-10 14:07:31 +0800

    完成布局调整

--- gui.py
+++ gui.py
@@ -5,28 +5,45 @@ from job import JobCrawler
 
 root = tk.Tk()
 root.title('51job')
-root.geometry("300x250")
+root.geometry("500x260")
 root.resizable(0, 0)
 
-ttk.Label(root, text="输入选好条件后的网址").pack(expand=1)
+# 提示标签
+ttk.Label(root, text="输入选好条件后的网址:").pack(pady=10)
 
+# 输入网址
 url = tk.StringVar()
-url_entry = ttk.Entry(root, textvariable=url)
-url_entry.pack()
+url_entry = ttk.Entry(root, textvariable=url, width=450)
+url_entry.pack(padx=30, pady=10)
 
 def _crawl(urls):
-    JobCrawler(urls).crawl()
+    # 运行任务并更新组件
+    jc = JobCrawler(urls)
+    state_label.configure(text='正在获取需要抓取的链接数量...')
+    count = jc.get_urls_count()
+    state_label.configure(text='需要抓取的链接数量为：{}'.format(count))
+    for index, url in enumerate(jc.urls_set):
+        job_details = ','.join(jc.crawl_one(url))
+        state_text = '{}/{}  {}'.format(index, count, job_details)
+        state_label.configure(text=state_text)
+    state_label.configure(text='查找完毕! 共找到{}条数据'.format(jc.result_count))
+    run.configure(text='查找')
 
 def crawl():
+    """button回调函数"""
     urls = [url.get().strip()]
     run.configure(text="查找中...")
+    # 新建线程
     t = threading.Thread(target=_crawl, args=(urls,))
     t.setDaemon(True)
     t.start()
 
+# 查找按钮
 run = ttk.Button(root, text='查找', command=crawl)
-run.pack(expand=5)
+run.pack(pady=10)
+
+# 查找状态更新标签
+state_label = ttk.Label(root, text='')
+state_label.pack(pady=10)
 
 root.mainloop()
-
-# https://search.51job.com/list/200200,000000,0000,00,3,06,%2B,1,1.html?lang=c&stype=1&postchannel=0000&workyear=02&cotype=99&degreefrom=04&jobterm=99&companysize=02&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=
